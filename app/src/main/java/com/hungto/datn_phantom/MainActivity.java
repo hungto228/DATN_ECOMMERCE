6a318b648bd47c3fe8693c90b8bac752bc8c9275
hungto228@gmail.com <hungto228@gmail.com>
2021-01-24 22:41:06 +0700

    add file

---
+++ app/src/main/java/com/hungto/datn_phantom/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hungto.datn_phantom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
