a21b8c72e12e65d07dc884d6df9c95ea7f3d93af
hungto228@gmail.com <hungto228@gmail.com>
2021-01-26 09:34:15 +0700

    validate login, resgiter

--- app/src/main/java/com/hungto/datn_phantom/fragment/SignInFragment.java
+++ app/src/main/java/com/hungto/datn_phantom/fragment/SignInFragment.java
@@ -1,18 +1,32 @@
 package com.hungto.datn_phantom.fragment;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.hungto.datn_phantom.MainActivity;
 import com.hungto.datn_phantom.R;
 
 
@@ -28,6 +42,11 @@ public class SignInFragment extends Fragment {
     private EditText mEmail, mPassword;
     private Button mSignin;
     private FrameLayout parentframeLayout;
+    private ProgressBar pbSignIn;
+    private ImageButton mBackArrow;
+    private FirebaseAuth firebaseAuth;
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,16 +57,19 @@ public class SignInFragment extends Fragment {
         mDonHaveAccount = view.findViewById(R.id.tv_noAcount);
         mFogot = view.findViewById(R.id.tv_forgot);
         //edittext
-        mEmail=view.findViewById(R.id.edt_email);
-        mPassword=view.findViewById(R.id.edt_password);
-        mSignin =view.findViewById(R.id.btn_signIn);
-        parentframeLayout =getActivity().findViewById(R.id.frame_register);
-
+        mEmail = view.findViewById(R.id.edt_email);
+        mPassword = view.findViewById(R.id.edt_password);
+        mSignin = view.findViewById(R.id.btn_signIn);
+        parentframeLayout = getActivity().findViewById(R.id.frame_register);
+        pbSignIn = view.findViewById(R.id.pb_signIn);
+        mBackArrow = view.findViewById(R.id.btnImg_back_arrow);
+        //fire
+        firebaseAuth = FirebaseAuth.getInstance();
         return view;
     }
 
     @Override
-    public void onViewCreated(@NonNull  View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mDonHaveAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,13 +77,112 @@ public class SignInFragment extends Fragment {
                 setFragmentSignUp(new SignUpFragment());
             }
         });
+        mEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInput();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInput();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkMailAndPassWord();
+            }
+        });
+        mBackArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainIntent();
+            }
+        });
     }
 
     private void setFragmentSignUp(Fragment fragmentsignup) {
-        FragmentTransaction fragmentTransaction=getActivity().getSupportFragmentManager().beginTransaction();
-      fragmentTransaction.setCustomAnimations(R.anim.slide_from_right,R.anim.slideout_from_right);
-        fragmentTransaction.replace(parentframeLayout.getId(),fragmentsignup);
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_right, R.anim.slideout_from_right);
+        fragmentTransaction.replace(parentframeLayout.getId(), fragmentsignup);
         fragmentTransaction.commit();
+    }
+
+    private void checkInput() {
+        if (!TextUtils.isEmpty(mEmail.getText().toString())) {
+            if (!TextUtils.isEmpty(mPassword.getText().toString())) {
+                mSignin.setEnabled(true);
+                mSignin.setTextColor(Color.rgb(255, 255, 255));
+            } else {
+                mSignin.setEnabled(false);
+                mSignin.setTextColor(Color.argb(50, 255, 255, 255));
+            }
+        } else {
+            mSignin.setEnabled(false);
+            mSignin.setTextColor(Color.argb(50, 255, 255, 255));
+        }
+    }
+
+    private void checkMailAndPassWord() {
+        if (mEmail.getText().toString().matches(emailPattern)) {
+            if (mPassword.getText().length() >= 8) {
+
+                pbSignIn.setVisibility(View.VISIBLE);
+                mSignin.setEnabled(false);
+                mSignin.setTextColor(Color.argb(50, 255, 255, 255));
+
+                firebaseAuth.signInWithEmailAndPassword(mEmail.getText().toString(), mPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Intent mainIntent = new Intent(getActivity(), MainActivity.class);
+                                    startActivity(mainIntent);
+                                    getActivity().finish();
+                                } else {
+                                    pbSignIn.setVisibility(View.INVISIBLE);
+                                    mSignin.setEnabled(true);
+                                    mSignin.setTextColor(Color.rgb(255, 255, 255));
+
+                                    String error = task.getException().getMessage();
+                                    Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            } else {
+                Toast.makeText(getActivity(), "ten dang nhap hoac mat khau sai", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getActivity(), "ten dang nhap hoac mat khau sai", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void mainIntent() {
+        Intent mainIntent = new Intent(getActivity(), MainActivity.class);
+        startActivity(mainIntent);
+        getActivity().finish();
     }
 }
--- app/src/main/java/com/hungto/datn_phantom/view/splashActivity/SplashScreen.java
+++ app/src/main/java/com/hungto/datn_phantom/view/splashActivity/SplashScreen.java
@@ -5,19 +5,37 @@ import android.os.SystemClock;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.hungto.datn_phantom.MainActivity;
 import com.hungto.datn_phantom.R;
 import com.hungto.datn_phantom.view.regiterActivity.RegiterActivity;
 
 
 public class SplashScreen extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        firebaseAuth=FirebaseAuth.getInstance();
         //sleep 3 seconds
         SystemClock.sleep(1000);
-        startActivity(new Intent(SplashScreen.this, RegiterActivity.class));
-        finish();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUser=firebaseAuth.getCurrentUser();
+        if(firebaseUser==null){
+            startActivity(new Intent(SplashScreen.this, RegiterActivity.class));
+            finish();
+        }else {
+            startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            finish();
+        }
     }
 }
