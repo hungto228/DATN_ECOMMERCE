755032d0bfca1a6ad05d35cf544ef180c4767b59
hungto228@gmail.com <hungto228@gmail.com>
2021-02-13 00:19:52 +0700

    add strip ads and language transtale

--- app/src/main/java/com/hungto/datn_phantom/fragment/HomeFragment.java
+++ app/src/main/java/com/hungto/datn_phantom/fragment/HomeFragment.java
@@ -1,21 +1,31 @@
 package com.hungto.datn_phantom.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.hungto.datn_phantom.R;
 import com.hungto.datn_phantom.adapter.CategoryAdapter;
+import com.hungto.datn_phantom.adapter.SliderAdapter;
 import com.hungto.datn_phantom.model.CategoryModel;
+import com.hungto.datn_phantom.model.SliderModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,27 +38,125 @@ public class HomeFragment extends Fragment {
     RecyclerView recyclerView;
     private CategoryAdapter categoryAdapter;
     Unbinder unbinder;
+    //banner slider
+    @BindView(R.id.viewpage_bannerSlider)
+    ViewPager banner;
+    SliderAdapter sliderAdapter;
+    private List<SliderModel> sliderModelList;
+    private int currentPage;
+    private Timer timer;
+    final private long DELAY_TIME = 3000;
+    final private long PERIOD_TIME = 3000;
+    //strip ads
+    @BindView(R.id.img_stripAds)
+    ImageView imgStrips;
+    @BindView(R.id.constrantlayout)
+    ConstraintLayout stripAdsContainer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        unbinder=ButterKnife.bind(this,root);
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
+        unbinder = ButterKnife.bind(this, root);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         recyclerView.setLayoutManager(linearLayoutManager);
-        List<CategoryModel> categoryModels=new ArrayList<CategoryModel>();
-        categoryModels.add(new CategoryModel("link","Home"));
-        categoryModels.add(new CategoryModel("link","Electrolics"));
-        categoryModels.add(new CategoryModel("link","Appliances"));
-        categoryModels.add(new CategoryModel("link","Furniture"));
-        categoryModels.add(new CategoryModel("link","Fashions"));
-        categoryModels.add(new CategoryModel("link","Toys"));
-        categoryModels.add(new CategoryModel("link","Wall Arts"));
-        categoryModels.add(new CategoryModel("link","Shoes"));
-        categoryAdapter=new CategoryAdapter(categoryModels);
+        List<CategoryModel> categoryModels = new ArrayList<CategoryModel>();
+        categoryModels.add(new CategoryModel("link", "Home"));
+        categoryModels.add(new CategoryModel("link", "Electrolics"));
+        categoryModels.add(new CategoryModel("link", "Appliances"));
+        categoryModels.add(new CategoryModel("link", "Furniture"));
+        categoryModels.add(new CategoryModel("link", "Fashions"));
+        categoryModels.add(new CategoryModel("link", "Toys"));
+        categoryModels.add(new CategoryModel("link", "Wall Arts"));
+        categoryModels.add(new CategoryModel("link", "Shoes"));
+        categoryAdapter = new CategoryAdapter(categoryModels);
         recyclerView.setAdapter(categoryAdapter);
         categoryAdapter.notifyDataSetChanged();
+        //banner
+        sliderModelList = new ArrayList<SliderModel>();
+        sliderModelList.add(new SliderModel(R.drawable.ic_email_screen,"#077AE4"));
+        sliderModelList.add(new SliderModel(R.drawable.banner_slider,"#077AE4"));
+        sliderModelList.add(new SliderModel(R.drawable.ic_email_red,"#077AE4"));
+        sliderModelList.add(new SliderModel(R.drawable.ic_email_screen,"#077AE4"));
+
+        sliderAdapter = new SliderAdapter(sliderModelList);
+        banner.setAdapter(sliderAdapter);
+        banner.setClipToPadding(false);
+        banner.setPageMargin(20);
+
+        banner.setCurrentItem(currentPage);
+        ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == ViewPager.SCROLL_STATE_IDLE) {
+                    pageLooper();
+                }
+            }
+        };
+        banner.addOnPageChangeListener(onPageChangeListener);
+        startBannerSlideShow();
+        banner.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                pageLooper();
+                stopBannerSlideShow();
+                if(event.getAction()==MotionEvent.ACTION_UP){
+                    startBannerSlideShow();
+                }
+                return false;
+            }
+        });
+
+        // Strip ads
+        imgStrips.setImageResource(R.drawable.banner_slider);
+        stripAdsContainer.setBackgroundColor(Color.parseColor("#000000"));
         return root;
+    }
+
+    //banner
+    private void pageLooper() {
+        if (currentPage == sliderModelList.size() - 2) {
+            currentPage = 2;
+            banner.setCurrentItem(currentPage, false);
+        }
+        if (currentPage == 1) {
+            currentPage = sliderModelList.size() - 3;
+            banner.setCurrentItem(currentPage, false);
+        }
+
+    }
+
+    private void startBannerSlideShow() {
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage >= sliderModelList.size()) {
+                    currentPage = 1;
+                }
+                banner.setCurrentItem(currentPage++, true);
+            }
+        };
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        },DELAY_TIME,PERIOD_TIME);
+    }
+    private void stopBannerSlideShow(){
+        timer.cancel();
     }
 }
