6fb5ef26c088692738740b2a4a2833b2e4be9e03
hungto228@gmail.com <hungto228@gmail.com>
2021-02-25 01:09:10 +0700

    add specification & bug

---
+++ app/src/main/java/com/hungto/datn_phantom/adapter/ProductDetailAdapter.java
@@ -0,0 +1,44 @@
+package com.hungto.datn_phantom.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.hungto.datn_phantom.fragment.ProductDescriptionFragment;
+import com.hungto.datn_phantom.fragment.ProductSpecificationFragment;
+
+public class ProductDetailAdapter extends FragmentPagerAdapter {
+    private int totalTabs;
+
+    public ProductDetailAdapter(@NonNull FragmentManager fm, int totalTabs) {
+        super(fm);
+        this.totalTabs = totalTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                ProductDescriptionFragment productDescriptionFragment=new ProductDescriptionFragment();
+                return productDescriptionFragment;
+            case 1:
+                ProductSpecificationFragment productSpecificationFragment=new ProductSpecificationFragment();
+                return  productSpecificationFragment;
+            case 2:
+                ProductDescriptionFragment productDescriptionFragment1=new ProductDescriptionFragment();
+                return productDescriptionFragment1;
+
+            default:
+                return null;
+        
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
