bab7ce17db9c79975359626ef4a5f2f7c78fed30
hungto228@gmail.com <hungto228@gmail.com>
2021-05-09 10:19:24 +0700

    design display userinfor

---
+++ app/src/main/java/com/hungto/datn_phantom/view/updateUserInfor/UpdateUserInfoActivity.java
@@ -0,0 +1,16 @@
+package com.hungto.datn_phantom.view.updateUserInfor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hungto.datn_phantom.R;
+
+public class UpdateUserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_user_info);
+    }
+}
