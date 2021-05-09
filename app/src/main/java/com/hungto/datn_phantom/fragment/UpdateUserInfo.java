d6d9e5a853ed522ecffebe3a8c6b1d5d759f647d
hungto228@gmail.com <hungto228@gmail.com>
2021-05-09 17:32:00 +0700

    select profile photo

--- app/src/main/java/com/hungto/datn_phantom/fragment/UpdateUserInfo.java
+++ app/src/main/java/com/hungto/datn_phantom/fragment/UpdateUserInfo.java
@@ -1,19 +1,49 @@
 package com.hungto.datn_phantom.fragment;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.firestore.CollectionReference;
 import com.hungto.datn_phantom.R;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
 
 
 public class UpdateUserInfo extends Fragment {
 
 
+    @BindView(R.id.edt_oldPassword)
+    MaterialEditText mOldPassWordEdt;
+    @BindView(R.id.edt_newPassword)
+    MaterialEditText mNewPassWordEdt;
+    @BindView(R.id.edt_conflim)
+    MaterialEditText mConfirmPassordEdt;
+    @BindView(R.id.btn_update)
+    Button mUpdatebtn;
+
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     public UpdateUserInfo() {
         // Required empty public constructor
@@ -23,6 +53,86 @@ public class UpdateUserInfo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_user_info, container, false);
+        View root = inflater.inflate(R.layout.fragment_update_user_info, container, false);
+        mOldPassWordEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInput();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mNewPassWordEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInput();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mConfirmPassordEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInput();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return root;
+    }
+
+    private void checkInput() {
+        if (!TextUtils.isEmpty(mOldPassWordEdt.getText()) && mOldPassWordEdt.length() >= 8) {
+            if (!TextUtils.isEmpty(mNewPassWordEdt.getText()) && mNewPassWordEdt.length() >= 8) {
+                if (!TextUtils.isEmpty(mConfirmPassordEdt.getText()) && mConfirmPassordEdt.length() >= 8) {
+                    mUpdatebtn.setEnabled(true);
+                    mUpdatebtn.setTextColor(Color.rgb(255, 255, 255));
+                } else {
+                    mUpdatebtn.setEnabled(false);
+                    mUpdatebtn.setTextColor(Color.argb(50, 255, 255, 255));
+                }
+            } else {
+                mUpdatebtn.setEnabled(false);
+                mUpdatebtn.setTextColor(Color.argb(50, 255, 255, 255));
+            }
+        } else {
+            mUpdatebtn.setEnabled(false);
+            mUpdatebtn.setTextColor(Color.argb(50, 255, 255, 255));
+        }
+    }
+
+    private void checkEmailAndPassword() {
+        Drawable customErrorIcon = getResources().getDrawable(R.drawable.ic_warning_yellow);
+        customErrorIcon.setBounds(0, 0, customErrorIcon.getIntrinsicWidth(), customErrorIcon.getIntrinsicHeight());
+
+        if (mNewPassWordEdt.getText().toString().equals(mConfirmPassordEdt.getText().toString())) {
+            //update passsworrd
+        } else {
+            mConfirmPassordEdt.setError("Email không hợp lệ !", customErrorIcon);
+        }
     }
 }
--- app/src/main/java/com/hungto/datn_phantom/view/updateUserInfor/UpdateUserInfoActivity.java
+++ app/src/main/java/com/hungto/datn_phantom/view/updateUserInfor/UpdateUserInfoActivity.java
@@ -1,16 +1,82 @@
 package com.hungto.datn_phantom.view.updateUserInfor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
+import com.google.android.material.tabs.TabLayout;
 import com.hungto.datn_phantom.R;
+import com.hungto.datn_phantom.fragment.ResetPassWordFragment;
+import com.hungto.datn_phantom.fragment.UpdateInfoFragment;
+import com.hungto.datn_phantom.fragment.UpdateUserInfo;
+
+import butterknife.BindView;
 
 public class UpdateUserInfoActivity extends AppCompatActivity {
+
+    @BindView(R.id.tablaout_update_user)
+    TabLayout tabLayout;
+    @BindView(R.id.frame_userInfo)
+    FrameLayout frameLayout;
+    private UpdateInfoFragment updateInfoFragment;
+    private UpdateUserInfo updateUserInfo;
+    private String name;
+    private String email;
+    private String photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_user_info);
+
+        //updateInfor fragment
+        updateInfoFragment = new UpdateInfoFragment();
+        updateUserInfo = new UpdateUserInfo();
+        //getintent accountfragment
+        name = getIntent().getStringExtra("Name");
+        email = getIntent().getStringExtra("Email");
+        photo = getIntent().getStringExtra("Photo");
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    setFragment(updateInfoFragment, true);
+                }
+                if (tab.getPosition() == 1) {
+                    setFragment(updateUserInfo, false);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        tabLayout.getTabAt(0).select();
+        setFragment(updateInfoFragment,true);
+
+
+    }
+
+    private void setFragment(Fragment fragment, boolean setBundle) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        if (setBundle) {
+            Bundle bundle = new Bundle();
+            bundle.putString("Name", name);
+            bundle.putString("Email", email);
+            bundle.putString("Photo", photo);
+            updateInfoFragment.setArguments(bundle);
+        }
+        fragmentTransaction.replace(frameLayout.getId(), fragment);
+        fragmentTransaction.commit();
     }
 }
